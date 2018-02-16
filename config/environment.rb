fdd8f8d926304a7261fe3835d16ea718c2b60c56
Rauof M <abdelrauof197@gmail.com>
2018-02-16 21:01:16 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,5 @@
 #require_all 'app'
 require_relative "../app/controllers/application_controller.rb"
 require_relative "../app/models/dog.rb"
-require_relative "../"
+#require_relative "../app/views/index.erb"
 require "sinatra"
